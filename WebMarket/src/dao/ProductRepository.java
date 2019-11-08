b07a6a6e5432afdee5c04f1f5316b3efc6bfd08f
USER <USER@AMP-17>
2019-11-08 12:51:47 +0900

    commit

--- WebMarket/src/dao/ProductRepository.java
+++ WebMarket/src/dao/ProductRepository.java
@@ -1,11 +1,29 @@
 package dao;
 
+import java.util.AbstractList;
 import java.util.ArrayList;
 
 import dto.Product;
 
+public Product getProductById(String productId) {
+	Product productById = null;
+	
+	for (int i = 0; i < listOfProducts.size(); i++) {
+		Product product = listOfProducts.get(i);
+		if (product != null && product.getProductId() != null && product.getProductId().equals(productId)) {
+			productById = product;
+			break;
+		}
+	}
+	return productById;
+}
+
 public class ProductRepository {
 	private ArrayList<Product> listOfProducts = new ArrayList<Product>();
+	
+	public AbstractList<Product> getAllProducts() {
+		return listOfProducts;
+	}
 	
 	public ProductRepository() {
 		Product phone = new Product("P1234","iPhone 6s", 800000);
@@ -20,5 +38,17 @@ public class ProductRepository {
 		notebook.setCategory("Notebbok");
 		notebook.setManufacturer("LG");
 		notebook.setUnitsInStock(1000);
+		notebook.setCondition("Refurbished");
+		
+		Product tablet = new Product("P1236", "Galaxy Tab S", 900000);
+		tablet.setDescription("212.8*125.6*6.6mm, Super AMOLED display, Octa-Core processor");
+		tablet.setCategory("Tablet");
+		tablet.setManufacturer("Samsung");
+		tablet.setUnitsInStock(1000);
+		tablet.setCondition("old");
+		
+		listOfProducts.add(phone);
+		listOfProducts.add(notebook);
+		listOfProducts.add(tablet);
 	}
 }
