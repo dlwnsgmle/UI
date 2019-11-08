f435e8d26028b3998a5c74ebf4da2c383543300f
USER <USER@AMP-17>
2019-11-08 16:32:29 +0900

    commit

--- WebMarket/src/dao/ProductRepository.java
+++ WebMarket/src/dao/ProductRepository.java
@@ -5,21 +5,18 @@ import java.util.ArrayList;
 
 import dto.Product;
 
-public Product getProductById(String productId) {
-	Product productById = null;
-	
-	for (int i = 0; i < listOfProducts.size(); i++) {
-		Product product = listOfProducts.get(i);
-		if (product != null && product.getProductId() != null && product.getProductId().equals(productId)) {
-			productById = product;
-			break;
-		}
-	}
-	return productById;
-}
 
 public class ProductRepository {
 	private ArrayList<Product> listOfProducts = new ArrayList<Product>();
+	private static ProductRepository instance = new ProductRepository();
+	
+	public static ProductRepository getInstance() {
+		return instance;
+	}
+	
+	public void addProduct(Product product) {
+		listOfProducts.add(product);
+	}
 	
 	public AbstractList<Product> getAllProducts() {
 		return listOfProducts;
