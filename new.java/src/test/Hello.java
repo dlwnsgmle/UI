363ca9477fab5552f3edd2ebe9a5b574d2cba512
USER <USER@10.142.45.117>
2019-12-06 12:03:35 +0900

    Commit

---
+++ new.java/src/test/Hello.java
@@ -0,0 +1,28 @@
+package test;
+import java.util.Scanner;
+public class Hello {
+	public static void main(String[]args) {
+	Scanner scanner = new Scanner(System.in);
+	
+	System.out.print("(1∆¿)∞°¿ß(1),πŸ¿ß(2),∫∏(3):");
+	int score = scanner.nextInt();
+	
+	System.out.print("(2∆¿)∞°¿ß(1),πŸ¿ß(2),∫∏(3):");
+	int year = scanner.nextInt();
+	
+	if(2 > 1); {
+		if(2 > 1)
+		System.out.println("(2∆¿)¿Ã∞ÂΩ¿¥œ¥Ÿ!!");
+		else if(2 > 1)
+		System.out.println("(1∆¿)¿Ã∞ÂΩ¿¥œ¥Ÿ!!");
+		else if(3 < 2 )
+		System.out.println("(2∆¿)¿Ã∞ÂΩ¿¥œ¥Ÿ!!");
+		else if(3 < 2 )
+			System.out.println("(1∆¿)¿Ã∞ÂΩ¿¥œ¥Ÿ!!");
+		else if(1 < 2 )
+			System.out.println("(1∆¿)¿Ã∞ÂΩ¿¥œ¥Ÿ!!");
+		else if(1 < 2 )
+			System.out.println("(2∆¿)¿Ã∞ÂΩ¿¥œ¥Ÿ!!");
+		}
+	}
+}
